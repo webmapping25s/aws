028000d6cb3908112366793e480e762dab943060
klaus <klaus.foerster@uibk.ac.at>
2025-05-07 18:16:08 +0200

    Daten laden und Icons für Wetterstationen

--- main.js
+++ main.js
@@ -39,6 +39,16 @@ async function loadStations(url) {
     let jsondata = await response.json();
 
     // Wetterstationen mit Icons und Popups
-
+    L.geoJSON(jsondata, {
+        pointToLayer: function(feature, latlng) {
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: "icons/wifi.png",
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -37]
+                })
+            });
+        },
+    }).addTo(overlays.stations)
 }
 loadStations("https://static.avalanche.report/weather_stations/stations.geojson");
