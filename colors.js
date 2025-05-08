6209abfbe61fa17876c32cb31bfe1fe0658a2f65
Klaus Förster <klaus.foerster@uibk.ac.at>
2025-05-08 17:28:05 +0200

    color ramp fuer temperatur definiert

---
+++ colors.js
@@ -0,0 +1,17 @@
+const COLORS = {
+    temperature: [
+        { min: -50, max: -25, color: "#9f80ff" },
+        { min: -25, max: -20, color: "#784cff" },
+        { min: -20, max: -15, color: "#0f5abe" },
+        { min: -15, max: -10, color: "#1380ff" },
+        { min: -10, max: -5, color: "#19cdff" },
+        { min: -5, max: 0, color: "#8fffff" },
+        { min: 0, max: 5, color: "#b0ffbc" },
+        { min: 5, max: 10, color: "#ffff73" },
+        { min: 10, max: 15, color: "#ffbe7d" },
+        { min: 15, max: 20, color: "#ff9b41" },
+        { min: 20, max: 25, color: "#ff5a41" },
+        { min: 25, max: 30, color: "#ff1e23" },
+        { min: 30, max: 50, color: "#fa3c96" },
+    ],
+};
