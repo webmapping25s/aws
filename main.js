bb5eed3395d61eed5c0311533e54c5fe8d71c4d4
Klaus Förster <klaus.foerster@uibk.ac.at>
2025-05-08 16:08:12 +0200

    popup um einige Werte erweitert mit fallback fuer undefined

--- main.js
+++ main.js
@@ -52,6 +52,13 @@ async function loadStations(url) {
         onEachFeature: function(feature, layer) {
             layer.bindPopup(`
                 <h4>${feature.properties.name} (${feature.geometry.coordinates[2]}m)</h4>
+                <ul>
+                  <li>Lufttemperatur (C) ${feature.properties.LT !== undefined ? feature.properties.LT : "-"}</li>
+                  <li>Relative Luftfeuchte (%) ${feature.properties.RH  || "-"}</li>
+                  <li>Windgeschwindigkeit (km/h) ${feature.properties.WG || "-"}</li>
+                  <li>Schneehöhe (cm) ${feature.properties.HS || "-"}</li>
+                </ul>
+                <span></span>
             `);
         }
     }).addTo(overlays.stations)
